bbcb6812ecfc5d1aa318a454def1bb16cdcd0ee5
Jamil <aydarkurbanbaev1406@gmail.com>
2026-01-14 21:40:41 +0500

    V2

--- data.js
+++ data.js
@@ -1,6 +1,20 @@
 ﻿export const files = [
-  // Добавь имена файлов из папки files, например:
-  // "BSA00_Decomposition.ID_12345.zip",
+  "BSA00_Decomposition.ID_1361439-1-develop.zip",
+  "BSA01_Stakeholders.ID_1361441-2-develop.zip",
+  "BSA02_Requirements.ID_1361442-3-develop.zip",
+  "BSA03_HowToRequirements.ID_1361443-Team_TL_philipet.5dfcc591_50cc_4d22-1-develop.zip",
+  "BSA04_Domains.ID_1361447-1-develop.zip",
+  "BSA05_Diagrams.ID_1361446-1-develop.zip",
+  "BSA06_BPMN.ID_1361449-Team_TL_thanosbe.904e0465_f0b2_4917-1-develop.zip",
+  "BSA07_UserStory.ID_1361451-1-develop.zip",
+  "C3_SimpleBashUtils.ID_353513-1-develop.zip",
+  "CbS1_Networking_basics_Part_1.ID_1521088-1-develop.zip",
+  "PjM1_ProjPM.ID_1577615-1-develop.zip",
+  "PjM2_LifeCycle.ID_1577616-1-develop.zip",
+  "QA1_Foundation.ID_1572541-1-develop.zip",
+  "QA2_Test_artifacts.ID_1572542-1-develop.zip",
+  "UX_UI1_Intro.ID_1577560-1-develop.zip",
+  "UX_UI2_UX_Design_Basics.ID_1577581-1-develop.zip",
 ];
 
 export const groupOrder = ["BSA", "QA", "UX_UI", "PjM", "CbS", "C", "Misc"];
